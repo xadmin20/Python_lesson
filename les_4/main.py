7ae6d07b9f8dfc1fb8f01afd0f60f300977b20bf
Nick <74838258+xadmin20@users.noreply.github.com>
2022-10-19 10:35:58 +0300

    Урок 4. Задача 2

--- les_4/main.py
+++ les_4/main.py
@@ -2,7 +2,6 @@
 def mnoz(n):
     if n <= 1:
         return "Задайте натуральное число больше 1!"
-        mnoz(n)
     else:
         i = 2 # первое простое число
         new_list = []
@@ -17,4 +16,17 @@ def mnoz(n):
         return f"Ответ: {n_main} ---> {new_list}"
 
 
-print(mnoz(int(input("Задайте натуральное число N: "))))
+# print(mnoz(int(input("Задайте натуральное число N: "))))
+
+# задача 2 . Задайте последовательность чисел.
+# Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+def num(l):
+    l = list(map(int, l))
+    print(f"Исходный список: {l}")
+    new_list = []
+    for i in l:
+        if i not in new_list:
+            new_list.append(i)
+    return f"Список из неповторяющихся элементов: {new_list}"
+
+print(num(input("Введите числа через пробел:\n").split(" ")))
